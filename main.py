9053675bd3cc173f2ec76cfb91e29893cc8554b7
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-05-16 07:15:28 +0200

    Creating a TODO list

---
+++ main.py
@@ -0,0 +1,8 @@
+#   TODO:
+#   1.Auth system
+#   2. DB for tickets, users, devices
+#       2.a: Users: 1st name, 2nd name, middle name, username, department, point to tickets and devices
+#       2.b: Tickets: ticket number, description, state, assigned SVD and user, affected user
+#       2.c: Devices: device type, owner, start use, warranty end, state (in use, loaner, in warehouse)
+#   3. Show tickets assigned to me and my team
+#   4.
