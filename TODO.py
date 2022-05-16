f95474bc86b63fc6d9bdd95832553b65e76a7b03
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-05-16 10:09:42 +0200

    Django start

--- TODO.py
+++ TODO.py
@@ -1,8 +1,11 @@
 #   TODO:
-#   1.Auth system
+#   1.Auth system - auth0 or maybe built-in django one?
 #   2. DB for tickets, users, devices
 #       2.a: Users: 1st name, 2nd name, middle name, username, department, point to tickets and devices
-#       2.b: Tickets: ticket number, description, state, assigned SVD and user, affected user
+#       2.b: Tickets: ticket number, description, state, assigned SVD and user, affected user, item
 #       2.c: Devices: device type, owner, start use, warranty end, state (in use, loaner, in warehouse)
 #   3. Show tickets assigned to me and my team
-#   4.
+#   4. Different pages for showing all tickets, handling tickets/users/devices
+#   5. Filtering tickets
+#   6. KB system?
+
