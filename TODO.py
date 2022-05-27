6c98226eb9f09698ae2b3308de3d26aa6d83d759
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-05-27 10:33:14 +0200

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -13,6 +13,9 @@
 # Add device to DB:
 # device = Devices(users=Users.objects.get(username="asd"), owner="asd", purchase_date=timezone.now(), warranty=timezone.now(), node_id="xyz")
 #device.save()
+#Search device based on username with users__username    - double _ is to be used to mark relationship in the DB if we need to grab from another model:
+#Devices.objects.filter(users__username="ASanyiAkiBela")
+
 
 
 
