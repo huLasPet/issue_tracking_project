6756e3c9714b75d4efd70017ca80d6772a6e8eee
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-06-01 11:37:48 +0200

    Testing page created for the DB

--- ticket_project/ticket_app/views.py
+++ ticket_project/ticket_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Users, Tickets, Devices
 # Create your views here.
 
 def index(request):
     return HttpResponse("Startpage for the issue tracker.")
+
+def test(request):
+    users = Users.objects.order_by('id')
+    output = ', '.join([user.username for user in users])
+    ticket = Tickets.objects.filter(affected_user="asd")
+    return HttpResponse("Users: %s" % output + "  ||| Ticket #: %s" % ticket[0].affected_user)
