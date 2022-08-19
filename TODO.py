8fc3615074007f6cc564dcd41109d28bbf02b10d
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-08-19 12:37:35 +0200

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,9 +1,9 @@
 #   TODO:
 #   6. KB system?
-#   7. Option to download data as an Excel file
 #   9. Instead of many API functions, have only 1 that gets all its data from the <> in the address -
 #       like <all_ticket> for all tickets or <one_ticket>/<ticket_number> for just one
 #       https://stackoverflow.com/questions/14351048/django-optional-url-parameters
+#   10. Add option to filter tables on the site
 
 
 # Add device to DB:
@@ -13,9 +13,3 @@
 #Devices.objects.filter(users__username="ASanyiAkiBela")
 
 
-import xlsxwriter
-
-workbook = xlsxwriter.Workbook('hello.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'Hello world')
-workbook.close()
