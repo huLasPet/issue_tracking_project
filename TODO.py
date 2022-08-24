e2ccdcbd7cc70a8a95373fa38350b88437c3f572
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-08-24 19:59:15 +0200

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -6,10 +6,6 @@
 #   10. Add option to filter tables on the site
 
 
-# Add device to DB:
-# device = Devices(users=Users.objects.get(username="asd"), owner="asd", purchase_date=timezone.now(), warranty=timezone.now(), node_id="xyz")
-#device.save()
-#Search device based on username with users__username    - double _ is to be used to mark relationship in the DB if we need to grab from another model:
-#Devices.objects.filter(users__username="ASanyiAkiBela")
+#uvicorn ticket_project.asgi:application --reload --host IP
 
 
