d89682d726e71fb521516457f532dc2b27d79846
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-08-25 15:05:31 +0200

    Basic table responsiveness

--- ticket_project/ticket_project/settings.py
+++ ticket_project/ticket_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-64@wrdk3*fhpw75wrvbk6_^@nx=)zm(9&pmau#2!7m9w6^f3v8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.50.220", "0.0.0.0"]
+ALLOWED_HOSTS = ["192.168.50.220", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
